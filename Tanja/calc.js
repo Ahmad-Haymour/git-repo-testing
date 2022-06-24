cd18dd43942b4462be5cea6c0e4b63aadfb8d2dc
Tatjana <bonktaniya@yahoo.com>
2022-06-24 20:12:06 +0200

    f

---
+++ Tanja/calc.js
@@ -0,0 +1,19 @@
+const rS = require("readline-sync");
+let a, b, c;
+a = parseInt(rS.question("a="));
+b = parseInt(rS.question("b="));
+c = rS.question("c=");
+function calculate(a, b, c) {
+  if (c == "+") {
+    return a + b;
+  } else if (c == "-") {
+    return a - b;
+  } else if (c == "*") {
+    return a * b;
+  } else if (c == "/") {
+    return a / b;
+  } else {
+    return false;
+  }
+}
+console.log(calculate(a, b, c));
